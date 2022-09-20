f936de384f85faedf0caa44da09f5d2ef0d953af
Arshad Nasim <arshad.nasim@staples.com>
2022-09-20 23:44:35 +0530

    Added Code for security

--- emp-basic-info-app/src/main/java/com/emp/app/config/SecurityConfig.java
+++ emp-basic-info-app/src/main/java/com/emp/app/config/SecurityConfig.java
@@ -1,16 +1,20 @@
 package com.emp.app.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SuppressWarnings("deprecation")
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private OncePerRequestFilterImpl oncePerRequestFilterImpl;
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(10);
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().authorizeRequests().anyRequest().authenticated().and()
+				.addFilterBefore(oncePerRequestFilterImpl, UsernamePasswordAuthenticationFilter.class);
 	}
 }
